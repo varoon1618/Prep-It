667a103c6d857bbf4577e71da9d0e007dc12c014
varoon1618 <114831390+varoon1618@users.noreply.github.com>
2023-06-12 19:46:14 +0100

    App generates and calls api

--- app/src/main/java/com/example/firstapp/GenerateMealPlan.java
+++ app/src/main/java/com/example/firstapp/GenerateMealPlan.java
@@ -5,11 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -98,21 +110,56 @@ public class GenerateMealPlan extends AppCompatActivity {
         spinner.setAdapter(adapter);
     }
 
-    public void generateAPI(String mealType) {
+    public String generateAPI(String mealType) {
+        String carbs="";
+        String fats="";
+        String protein="";
         String api = "https://api.edamam.com/api/recipes/v2?type=public&app_id=6d289bce&app_key=e3c92bc15780f1669b17ef15df5fc7a1";
         String health = preferenceMap.get(preferences.getSelectedItem().toString());
         api = api+health;
+        String cuisineType = cuisineMap.get(bfastCuisine.getSelectedItem().toString());
+        api = api+cuisineType;
         if(mealType.equals("breakfast")){
-            String cuisineType = cuisineMap.get(bfastCuisine.getSelectedItem().toString());
-            api = api+cuisineType;
+            api = api+"&mealType=Breakfast";
+            int idealCalories = Math.toIntExact(Math.round(0.3 * MainActivity.macros.get(0).getIdealCalories()));
+            String calories = "&calories="+String.valueOf(idealCalories-30)+"-"+String.valueOf(idealCalories+30);
+            api = api+calories;
+            int idealCarbs = Math.toIntExact(Math.round(0.3 * MainActivity.macros.get(0).getIdealCarbs()));
+            carbs = "&nutrients%5BCHOCDF.net%5D="+String.valueOf(idealCarbs-30)+"-"+String.valueOf(idealCarbs+30);
+            int idealFats = Math.toIntExact(Math.round(0.3 * MainActivity.macros.get(0).getIdealFats()));
+            fats = "&nutrients%5BFAT%5D="+String.valueOf(idealFats-30)+"-"+String.valueOf(idealFats+30);
+            int idealProt = Math.toIntExact(Math.round(0.3 * MainActivity.macros.get(0).getIdealProtein()));
+            protein = "&nutrients%5BPROCNT%5D="+String.valueOf(idealProt-30)+"-"+String.valueOf(idealProt+30);
         }else if(mealType.equals("lunch")){
-            String cuisineType = cuisineMap.get(lunchCuisine.getSelectedItem().toString());
-            api = api+cuisineType;
+            api = api+"&mealType=Dinner&mealType=Lunch";
+            int idealCalories = Math.toIntExact(Math.round(0.4 * MainActivity.macros.get(0).getIdealCalories()));
+            String calories = "&calories="+String.valueOf(idealCalories-10)+"-"+String.valueOf(idealCalories+10);
+            api = api+calories;
+            int idealCarbs = Math.toIntExact(Math.round(0.4 * MainActivity.macros.get(0).getIdealCarbs()));
+            carbs = "&nutrients%5BCHOCDF.net%5D="+String.valueOf(idealCarbs-30)+"-"+String.valueOf(idealCarbs+30);
+            int idealFats = Math.toIntExact(Math.round(0.4 * MainActivity.macros.get(0).getIdealFats()));
+            fats = "&nutrients%5BFAT%5D="+String.valueOf(idealFats-30)+"-"+String.valueOf(idealFats+30);
+            int idealProt = Math.toIntExact(Math.round(0.4 * MainActivity.macros.get(0).getIdealProtein()));
+            protein = "&nutrients%5BPROCNT%5D="+String.valueOf(idealProt-30)+"-"+String.valueOf(idealProt+30);
         }else{
-            String cuisineType = cuisineMap.get(dinnerCuisine.getSelectedItem().toString());
-            api = api+cuisineType;
+            api = api+"&mealType=Dinner&mealType=Lunch";
+            int idealCalories = Math.toIntExact(Math.round(0.3 * MainActivity.macros.get(0).getIdealCalories()));
+            String calories = "&calories="+String.valueOf(idealCalories-10)+"-"+String.valueOf(idealCalories+10);
+            api = api+calories;
+            int idealCarbs = Math.toIntExact(Math.round(0.3 * MainActivity.macros.get(0).getIdealCarbs()));
+            carbs = "&nutrients%5BCHOCDF.net%5D="+String.valueOf(idealCarbs-30)+"-"+String.valueOf(idealCarbs+30);
+            int idealFats = Math.toIntExact(Math.round(0.3 * MainActivity.macros.get(0).getIdealFats()));
+            fats = "&nutrients%5BFAT%5D="+String.valueOf(idealFats-30)+"-"+String.valueOf(idealFats+30);
+            int idealProt = Math.toIntExact(Math.round(0.3 * MainActivity.macros.get(0).getIdealProtein()));
+            protein = "&nutrients%5BPROCNT%5D="+String.valueOf(idealProt-30)+"-"+String.valueOf(idealProt+30);
         }
-
+        if(allergen.getText().toString().trim().length()>0){
+            String excluded = "&excluded="+allergen.getText().toString();
+            api = api+excluded;
+        }
+        api = api+"&random=true"+carbs+fats+protein;
+        Log.i("api",api);
+        return api;
     }
 
     public void populateCuisines(){
@@ -140,6 +187,68 @@ public class GenerateMealPlan extends AppCompatActivity {
         preferenceMap.put("Kosher","&health=kosher");
         preferenceMap.put("Pork free","&health=pork-free");
         preferenceMap.put("Keto","&health=keto");
+    }
+
+    /**
+     * Method to first call an api inputted and then parse the json api call and retrieve
+     * the details about all recipes that meet the required conditions (
+     * like cuisine type , calories , protein etc)
+     * Method also creates a recipe object and adds it to an arrayList recipes
+     */
+
+    public void getData(String api){
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        //Calling api request
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,api,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i("api","worked "+response.toString());
+                        try {
+                            JSONObject object = new JSONObject(response.toString());
+                            JSONArray hits = object.getJSONArray("hits"); //json array of all recipes
+                            for(int i=0;i<hits.length();i++){
+                                JSONObject recipe = new JSONObject(hits.get(i).toString()); // first recipe from the above array
+                                String name = recipe.getJSONObject("recipe").getString("label"); //gets name of recipe
+                                Log.i("label",name);
+                                String url = recipe.getJSONObject("recipe").getString("url");//gets url for recipe
+                                String string_yield = recipe.getJSONObject("recipe").getString("yield");//gets yield in form of string (yield here is the number of servings the recipe makes)
+                                double yield = Math.round(Double.parseDouble(string_yield)); // converts yield to double
+                                String string_calories = recipe.getJSONObject("recipe").getString("calories");//gets calories in form of string
+                                double calories = Math.round(Double.parseDouble(string_calories)/yield);//calories as double
+                                Log.i("yield",String.valueOf(yield));
+                                Log.i("calories", String.valueOf(calories));
+                                JSONArray ingredients = recipe.getJSONObject("recipe").getJSONArray("ingredientLines");// gets json array of all ingredients
+                                String string_carbs = recipe.getJSONObject("recipe").getJSONObject("totalNutrients").getJSONObject("CHOCDF.net").getString("quantity");// gets the carbs in that recipe as a string
+                                double carbs = Math.round(Double.parseDouble(string_carbs)/yield); // divides carbs by yield to get total carbs in one serving
+                                String string_proteins = recipe.getJSONObject("recipe").getJSONObject("totalNutrients").getJSONObject("PROCNT").getString("quantity"); // gets proteins in that recipe as str
+                                double proteins = Math.round(Double.parseDouble(string_proteins)/yield); // again divides protein by yeild as above
+                                String string_fats = recipe.getJSONObject("recipe").getJSONObject("totalNutrients").getJSONObject("FAT").getString("quantity");// same thing with fats (See above)
+                                Log.i("carbs",String.valueOf(carbs));
+                                double fats = Math.round(Double.parseDouble(string_fats)/yield);
+                                Log.i("carbs", String.valueOf(carbs));
+                                Log.i("protein", String.valueOf(proteins));
+                                Log.i("fats", String.valueOf(fats));
+                                Recipe food_item = new Recipe(name,yield,calories,carbs,proteins,fats,ingredients,url); // creates a new recipe object and adds them to an arraylist of recipes
+                                MainActivity.recipes.add(food_item);
+                            }
+
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("api","error: "+error.getLocalizedMessage());
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 }
--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -31,8 +31,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     // api request
-    String api = "https://api.edamam.com/api/recipes/v2?type=public&app_id=6d289bce&app_key=e3c92bc15780f1669b17ef15df5fc7a1&diet=high-protein&diet=low-carb&cuisineType=Indian&mealType=Dinner&mealType=Lunch&calories=100-300&random=true";
-    ArrayList<Recipe> recipes = new ArrayList<>(); //arraylist to hold all recipes that meet all requirements
+
+    public static ArrayList<Recipe> recipes = new ArrayList<>(); //arraylist to hold all recipes that meet all requirements
     public static ArrayList<User> users = new ArrayList<>(); //arraylist to store all users (should store only one user at a time)
     public static ArrayList<Macro> macros = new ArrayList<>();// araylist to store the macro value of the user(should store only one value at a time)
     private Button nutritionButton ; // button to navigate to screen where user is prompted to add information to calculate ideal macros
@@ -80,64 +80,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /**
-     * Method to parse the json api call and retrieve the details about all recipes that
-     * meet the required conditions (like cuisine type , calories , protein etc)
-     */
-    public void getData(){
-        RequestQueue queue = Volley.newRequestQueue(this);
 
-        //Calling api request
-        StringRequest stringRequest = new StringRequest(Request.Method.GET,api,
-                new Response.Listener<String>() {
 
-                    @Override
-                    public void onResponse(String response) {
-                        Log.i("api","worked "+response.toString());
-                        try {
-                            JSONObject object = new JSONObject(response.toString());
-                            JSONArray hits = object.getJSONArray("hits"); //json array of all recipes
-                            for(int i=0;i<hits.length();i++){
-                                JSONObject recipe = new JSONObject(hits.get(i).toString()); // first recipe from the above array
-                                String name = recipe.getJSONObject("recipe").getString("label"); //gets name of recipe
-                                Log.i("label",name);
-                                String url = recipe.getJSONObject("recipe").getString("url");//gets url for recipe
-                                String string_yield = recipe.getJSONObject("recipe").getString("yield");//gets yield in form of string (yield here is the number of servings the recipe makes)
-                                double yield = Math.round(Double.parseDouble(string_yield)); // converts yield to double
-                                String string_calories = recipe.getJSONObject("recipe").getString("calories");//gets calories in form of string
-                                double calories = Math.round(Double.parseDouble(string_calories)/yield);//calories as double
-                                Log.i("yield",String.valueOf(yield));
-                                Log.i("calories", String.valueOf(calories));
-                                JSONArray ingredients = recipe.getJSONObject("recipe").getJSONArray("ingredientLines");// gets json array of all ingredients
-                                String string_carbs = recipe.getJSONObject("recipe").getJSONObject("totalNutrients").getJSONObject("CHOCDF.net").getString("quantity");// gets the carbs in that recipe as a string
-                                double carbs = Math.round(Double.parseDouble(string_carbs)/yield); // divides carbs by yield to get total carbs in one serving
-                                String string_proteins = recipe.getJSONObject("recipe").getJSONObject("totalNutrients").getJSONObject("PROCNT").getString("quantity"); // gets proteins in that recipe as str
-                                double proteins = Math.round(Double.parseDouble(string_proteins)/yield); // again divides protein by yeild as above
-                                String string_fats = recipe.getJSONObject("recipe").getJSONObject("totalNutrients").getJSONObject("FAT").getString("quantity");// same thing with fats (See above)
-                                Log.i("carbs",String.valueOf(carbs));
-                                double fats = Math.round(Double.parseDouble(string_fats)/yield);
-                                Log.i("carbs", String.valueOf(carbs));
-                                Log.i("protein", String.valueOf(proteins));
-                                Log.i("fats", String.valueOf(fats));
-                                Recipe food_item = new Recipe(name,yield,calories,carbs,proteins,fats,ingredients,url); // creates a new recipe object and adds them to an arraylist of recipes
-                                recipes.add(food_item);
-                                Log.i("arraylist", String.valueOf(recipes.size()));
-                            }
-
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("api","error: "+error.getLocalizedMessage());
-            }
-        });
-
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }
 
 }
